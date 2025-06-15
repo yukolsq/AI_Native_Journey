0b33ab85c2401d760591542f833f0249b31391f8
yukolsq <yuko.ichihara0@gmail.com>
2025-06-15 12:08:44 -0400

    Initial commit of my new AI project

---
+++ welcome.py
@@ -0,0 +1,7 @@
+def print_welcome_message():
+    name = "Yuko"
+    message = f"Hello my name is {name}. I'm learning AI to become an awesome AI Native builder."
+    print(message)
+
+if __name__ == "__main__":
+    print_welcome_message() 
