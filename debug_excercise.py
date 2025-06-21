c28d9f297da363d6f166d119fe3bad87e7479e11
Yuko Ichihara <yuko.ichihara0@gmail.com>
2025-06-21 09:41:50 -0700

    Add files via upload

---
+++ debug_excercise.py
@@ -0,0 +1,15 @@
+# This script greets the user and calculates a "lucky number"
+
+user_name = input("Hello! What's your name? ")
+
+# This variable is intended to be used later
+# favorite_number = 7
+
+print(f"Nice to meet you, {user_name}!")
+
+# Attempt to calculate a lucky number
+lucky_number = favorite_number * 2
+
+print(f"Your lucky number is: {lucky_number}")
+
+print("Have a great day!")
