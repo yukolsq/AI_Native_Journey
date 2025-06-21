0e3432eea1d0ae3aee71e07f6def82b207ec361e
Yuko Ichihara <yuko.ichihara0@gmail.com>
2025-06-21 09:43:19 -0700

    Add files via upload

---
+++ error-ex1.py
@@ -0,0 +1,9 @@
+def print_welcome_message():
+    name = "Yuko"
+    message = f"Hello my name is {name. I'm learning AI to become an awesome AI Native builder."
+    print(message)
+
+if __name__ == "__main__":
+    print_welcome_message() 
+
+# this is a comment just to show git diffderences
